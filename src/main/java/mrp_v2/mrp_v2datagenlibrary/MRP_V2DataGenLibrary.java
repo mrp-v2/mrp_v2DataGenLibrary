dffb4f5e0a74def9fea5df612d51ea66690f9289
mrp-v2 <matthrewp@gmail.com>
2020-10-07 10:43:26 -0400

    created mod

---
+++ src/main/java/mrp_v2/mrp_v2datagenlibrary/MRP_V2DataGenLibrary.java
@@ -0,0 +1,9 @@
+package mrp_v2.mrp_v2datagenlibrary;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MRP_V2DataGenLibrary.ID) public class MRP_V2DataGenLibrary
+{
+    public static final String ID = "mrp_v2datagenlibrary";
+    public static final String DISPLAY_NAME = "MRP_V2DataGenLibrary";
+}
