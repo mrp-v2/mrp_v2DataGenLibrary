4b4175683967cad9b6bc7c010428b9144c0b2c6b
mrp-v2 <matthrewp@gmail.com>
2020-10-14 09:53:17 -0400

    added translation stuff

---
+++ src/main/java/mrp_v2/mrp_v2datagenlibrary/datagen/TranslationGenerator.java
@@ -0,0 +1,46 @@
+package mrp_v2.mrp_v2datagenlibrary.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.common.data.LanguageProvider;
+import org.apache.logging.log4j.LogManager;
+
+public abstract class TranslationGenerator extends LanguageProvider
+{
+    protected final String modid;
+
+    public TranslationGenerator(DataGenerator gen, String modid, String locale)
+    {
+        super(gen, modid, locale);
+        this.modid = modid;
+    }
+
+    public void add(ContainerType<?> key, String name)
+    {
+        add(key.getRegistryName().getNamespace() + ".container." + key.getRegistryName().getPath(), name);
+    }
+
+    public void add(ItemGroup key, String name)
+    {
+        TranslationTextComponent test = key.getGroupName() instanceof TranslationTextComponent ?
+                (TranslationTextComponent) key.getGroupName() :
+                null;
+        if (test != null)
+        {
+            add(test.getKey(), name);
+        } else
+        {
+            LogManager.getLogger()
+                    .warn("Could not make a translation for " +
+                            key +
+                            " because its groupName is not a TranslationTextComponent!");
+        }
+    }
+
+    @Override public String getName()
+    {
+        return super.getName() + ": " + this.modid;
+    }
+}
